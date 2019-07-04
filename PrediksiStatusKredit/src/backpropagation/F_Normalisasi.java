9063fbd4df652c1a92c919241fded942de67e4f1
Iqbal Zakariyah <iqbal@upm.ac.id>
2019-07-04 11:57:17 +0700

    bp

--- PrediksiStatusKredit/src/backpropagation/F_Normalisasi.java
+++ PrediksiStatusKredit/src/backpropagation/F_Normalisasi.java
@@ -23,7 +23,7 @@ import javax.swing.table.DefaultTableModel;
 
 @SuppressWarnings("unused")
 public class F_Normalisasi {
-	
+
 	public JFrame frameNormalisasi;
 	private JButton btnProses,btnBack,btnLoad,btnSave;
 	public static JTextField txtPath,txtdestination;	
@@ -122,6 +122,7 @@ public class F_Normalisasi {
 				}
 			}
 		});
+		
 		btnProses.setBounds(160, 400, 113, 40);
 		frameNormalisasi.getContentPane().add(btnProses);
 		
@@ -129,12 +130,12 @@ public class F_Normalisasi {
 		btnBack.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
 				F_Utama ft = new F_Utama();
 				ft.frameMain.setVisible(true);
 				frameNormalisasi.dispose();
 			}
 		});
+		
 		btnBack.setBounds(320, 400, 113, 40);
 		frameNormalisasi.getContentPane().add(btnBack);	
 		
@@ -144,175 +145,149 @@ public class F_Normalisasi {
 		frameNormalisasi.getContentPane().add(scrollPane);	
 	}
 	
-public void datanormalisasi() {    	 
-	fTesData = new float[2000][2000];    
-    String s = bacaFile("transformasi.csv");     
-    String[] has =s.split("\n");
-    
-    for(int i=0;i<has.length;i++){
-        String nilai[]=has[i].split(";");
-        jumkol=nilai.length;
-        for(int j=0;j<nilai.length;j++){
-            try{
-            	fTesData[i][j]=Float.valueOf(nilai[j]);
-            //
-            counterdata++;             
-            }catch(NumberFormatException e){                
-            }
-        }
-       
-    }
-}
-
-public static void tulisFile(String teks, String namaFile) {
-    try {
-        PrintWriter out = new PrintWriter(new BufferedWriter(
-                new FileWriter(namaFile, true)));
-        out.println(teks.toString());
-        out.close();
-    } catch (IOException e) {
-        System.out.println("Gagal menulis ke file " + namaFile);
-    }
-}
-private static float max(float[][] data,int j) {
-    float max = 0;
-    
-    int jumdat=counterdata/jumkol;
-    for(int i=0;i<jumdat;i++){
-         
-        if(data[i][j]>max){
-            max=data[i][j];
-             
-        }
-    }
-   //System.out.println(max);
-    return max;
-}
-
-private static float min(float[][] data,int j){
-    float min=1000000;
-            
-           int jumdat=counterdata/jumkol;
-           for(int i=0;i<jumdat;i++){
-               if(min>data[i][j]){
-               min=data[i][j];
-               }
-                
-       }
-        //System.out.println(min);   
-       return min;
-       
-   }
-
-public static void minMax(float[][] data){
-    float[][] newdata=new float[2000][2000];
-    String s,view;
-    int j,i,jumdat=counterdata/jumkol;
-    
-    int newmax= 1;
-    int newmin= 0;
-    for(j=0;j<jumkol;j++){
-    float max=max(data,j);
-    float min=min(data,j);
-    for(i=0;i<jumdat;i++){
-         
-        newdata[i][j] = ((data[i][j]-min)*(newmax-newmin))/((max-min)+newmin);
-        //newdata[i][j] = (int)(newdata[i][j]*100);
-    }
-  
-  }
-    DecimalFormat df = new DecimalFormat("#.##");
-    for(j=0;j<jumdat;j++){
-        s=""+newdata[j][0]+";"+newdata[j][1]+";"+newdata[j][2]+";"+newdata[j][3]+";"+newdata[j][4]+";"+newdata[j][5]+";"+newdata[j][6]+";"+newdata[j][7]+";"+newdata[j][8]+";"+newdata[j][9]+";"+newdata[j][10]+";"+newdata[j][11]+";"+newdata[j][12]+";"+newdata[j][13]+";"+newdata[j][14]+";"+newdata[j][15]+";"+newdata[j][16]+";"+newdata[j][17]+";"+data[j][jumkol-1];
-        view=""+df.format(newdata[j][0])+"\t"+df.format(newdata[j][1])+"\t"+df.format(newdata[j][2])+"\t"+df.format(newdata[j][3])+"\t"+df.format(newdata[j][4])+"\t"+df.format(newdata[j][5])+"\t"+df.format(newdata[j][6])+"\t"+df.format(newdata[j][7])+"\t"+df.format(newdata[j][8])+"\t"+df.format(newdata[j][9])+"\t"+df.format(newdata[j][10])+"\t"+df.format(newdata[j][11])+"\t"+df.format(newdata[j][12])+"\t"+df.format(newdata[j][13])+"\t"+df.format(newdata[j][14])+"\t"+df.format(newdata[j][15])+"\t"+df.format(newdata[j][16])+"\t"+df.format(newdata[j][17])+"\t"+data[j][jumkol-1]+"\n";
-        tulisFile(s,txtdestination.getText());
-        //textArea_Result.setText(textArea_Result.getText()+view);
-           //System.out.println(s);
-    }
-  }
-
-public static String bacaFile(String namaFile) {
-    BufferedReader br = null;
-    String stringHasil = "";
-
-    try {
-    	
-        String sCurrentLine;
-        br = new BufferedReader(new FileReader(namaFile));
-        while ((sCurrentLine = br.readLine()) != null) {
-            stringHasil = stringHasil + sCurrentLine + "\n";
-        }
-
-    } catch (IOException e) {
-        System.out.println("Gagal membaca file " + namaFile);
-    } finally {
-        try {
-            if (br != null)
-                br.close();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
-    }
-    return stringHasil;
-}
-public void hasiltabel(){
+	public void datanormalisasi() {
+		fTesData = new float[2000][2000];    
+	    String s = bacaFile("transformasi.csv");     
+	    String[] has =s.split("\n");
+	    
+	    for (int i=0;i<has.length;i++) {
+	        String nilai[]=has[i].split(";");
+	        jumkol=nilai.length;
+	        for (int j=0;j<nilai.length;j++) {
+	            try {
+	            	fTesData[i][j]=Float.valueOf(nilai[j]);
+	            	counterdata++;             
+	            } catch(NumberFormatException e){
+	            	// code
+	            }
+	        }
+	    }
+	}
 	
-	try {
-
-		
-        //BufferedReader br = new BufferedReader(new FileReader(new File("D:\\2019\\rrrrr.csv")));
-		BufferedReader br = new BufferedReader(new FileReader(txtdestination.getText().toString()));
-        List<String[]> elements = new ArrayList<String[]>();
-        String line = null;
-        while((line = br.readLine())!=null) {
-            String[] splitted = line.split(";");
-            elements.add(splitted);
-        }
-        br.close();
-
-       
-        String[] columNames = new String[] {
-                "Gaji", "TanggKel", "KMGRA", "KUR01" ,
-                "MKONF", "PMPKT", "KUR15", "KSUPM",
-                "KUR02", "MLGMK", "NPPPR", "PKMKA",
-                "PMPKA", "SukuBunga", "EIR", "HariTunggakan",
-                "LamaPinj", "TypeCKPN", "Status"
-                
-            };
-
-        Object[][] content = new Object[elements.size()][19];
-
-        for(int i=0; i<elements.size(); i++) {
-            content[i][0] = elements.get(i)[0];
-            content[i][1] = elements.get(i)[1];
-            content[i][2] = elements.get(i)[2];
-            content[i][3] = elements.get(i)[3];
-            content[i][4] = elements.get(i)[4];
-            content[i][5] = elements.get(i)[5];
-            content[i][6] = elements.get(i)[6];
-            content[i][7] = elements.get(i)[7];
-            content[i][8] = elements.get(i)[8];
-            content[i][9] = elements.get(i)[9];
-            content[i][10] = elements.get(i)[10];
-            content[i][11] = elements.get(i)[11];
-            content[i][12] = elements.get(i)[12];
-            content[i][13] = elements.get(i)[13];
-            content[i][14] = elements.get(i)[14];
-            content[i][15] = elements.get(i)[15];
-            content[i][16] = elements.get(i)[16];
-            content[i][17] = elements.get(i)[17];
-            content[i][18] = elements.get(i)[18];
-            
-            
-        }
-
-        table.setModel(new DefaultTableModel(content,columNames));
-
-        System.out.println(table.getModel().getValueAt(1, 1));
-
-    } catch (Exception ex) {
-        ex.printStackTrace();
-    }
-}
+	public static void tulisFile(String teks, String namaFile) {
+	    try {
+	    	PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(namaFile, true)));
+	        out.println(teks.toString());
+	        out.close();
+	    } catch (IOException e) {
+	        System.out.println("Gagal menulis ke file " + namaFile);
+	    }
+	}
 	
+	private static float max(float[][] data,int j) {
+	    float max = 0;
+	    int jumdat=counterdata/jumkol;
+	    for (int i=0;i<jumdat;i++) {
+	        if(data[i][j]>max){
+	            max=data[i][j];
+	        }
+	    }
+	    //System.out.println(max);
+	    return max;
+	}
+	
+	private static float min(float[][] data,int j) {
+	    float min=1000000;
+	    int jumdat=counterdata/jumkol;
+	    for (int i=0;i<jumdat;i++) {
+	    	if (min>data[i][j]){
+	    		min=data[i][j];
+	    	}
+	    }
+	    //System.out.println(min);   
+	    return min;
+	}
+	
+	public static void minMax(float[][] data){
+	    float[][] newdata=new float[2000][2000];
+	    String s,view;
+	    int j,i,jumdat=counterdata/jumkol;    
+	    int newmax= 1;
+	    int newmin= 0;
+	    for (j=0;j<jumkol;j++) {
+		    float max=max(data,j);
+		    float min=min(data,j);
+		    for (i=0;i<jumdat;i++) {
+		    	newdata[i][j] = ((data[i][j]-min)*(newmax-newmin))/((max-min)+newmin);
+		    	//newdata[i][j] = (int)(newdata[i][j]*100);
+		    }
+	    }
+	    DecimalFormat df = new DecimalFormat("#.##");
+	    for (j=0;j<jumdat;j++) {
+	    	s=""+newdata[j][0]+";"+newdata[j][1]+";"+newdata[j][2]+";"+newdata[j][3]+";"+newdata[j][4]+";"+newdata[j][5]+";"+newdata[j][6]+";"+newdata[j][7]+";"+newdata[j][8]+";"+newdata[j][9]+";"+newdata[j][10]+";"+newdata[j][11]+";"+newdata[j][12]+";"+newdata[j][13]+";"+newdata[j][14]+";"+newdata[j][15]+";"+newdata[j][16]+";"+newdata[j][17]+";"+data[j][jumkol-1];
+	        view=""+df.format(newdata[j][0])+"\t"+df.format(newdata[j][1])+"\t"+df.format(newdata[j][2])+"\t"+df.format(newdata[j][3])+"\t"+df.format(newdata[j][4])+"\t"+df.format(newdata[j][5])+"\t"+df.format(newdata[j][6])+"\t"+df.format(newdata[j][7])+"\t"+df.format(newdata[j][8])+"\t"+df.format(newdata[j][9])+"\t"+df.format(newdata[j][10])+"\t"+df.format(newdata[j][11])+"\t"+df.format(newdata[j][12])+"\t"+df.format(newdata[j][13])+"\t"+df.format(newdata[j][14])+"\t"+df.format(newdata[j][15])+"\t"+df.format(newdata[j][16])+"\t"+df.format(newdata[j][17])+"\t"+data[j][jumkol-1]+"\n";
+	        tulisFile(s,txtdestination.getText());
+	        //textArea_Result.setText(textArea_Result.getText()+view);
+	        //System.out.println(s);
+	    }
+	}
+	
+	public static String bacaFile(String namaFile) {
+	    BufferedReader br = null;
+	    String stringHasil = "";
+	    try {
+	        String sCurrentLine;
+	        br = new BufferedReader(new FileReader(namaFile));
+	        while ((sCurrentLine = br.readLine()) != null) {
+	            stringHasil = stringHasil + sCurrentLine + "\n";
+	        }
+	    } catch (IOException e) {
+	        System.out.println("Gagal membaca file " + namaFile);
+	    } finally {
+	    	try {
+	        	if (br != null)
+	                br.close();
+	        } catch (IOException ex) {
+	            System.out.println(ex.getMessage());
+	        }
+	    }
+	    return stringHasil;
+	}
+	
+	public void hasiltabel() {
+		try {
+	        //BufferedReader br = new BufferedReader(new FileReader(new File("D:\\2019\\rrrrr.csv")));
+			BufferedReader br = new BufferedReader(new FileReader(txtdestination.getText().toString()));
+	        List<String[]> elements = new ArrayList<String[]>();
+	        String line = null;
+	        while((line = br.readLine())!=null) {
+	        	String[] splitted = line.split(";");
+	        	elements.add(splitted);
+	        }
+	        br.close();
+	        String[] columNames = new String[] {
+	        		"Gaji", "TanggKel", "KMGRA", "KUR01" ,
+	                "MKONF", "PMPKT", "KUR15", "KSUPM",
+	                "KUR02", "MLGMK", "NPPPR", "PKMKA",
+	                "PMPKA", "SukuBunga", "EIR", "HariTunggakan",
+	                "LamaPinj", "TypeCKPN", "Status"
+	        };
+	        Object[][] content = new Object[elements.size()][19];
+	        for(int i=0; i<elements.size(); i++) {
+	            content[i][0] = elements.get(i)[0];
+	            content[i][1] = elements.get(i)[1];
+	            content[i][2] = elements.get(i)[2];
+	            content[i][3] = elements.get(i)[3];
+	            content[i][4] = elements.get(i)[4];
+	            content[i][5] = elements.get(i)[5];
+	            content[i][6] = elements.get(i)[6];
+	            content[i][7] = elements.get(i)[7];
+	            content[i][8] = elements.get(i)[8];
+	            content[i][9] = elements.get(i)[9];
+	            content[i][10] = elements.get(i)[10];
+	            content[i][11] = elements.get(i)[11];
+	            content[i][12] = elements.get(i)[12];
+	            content[i][13] = elements.get(i)[13];
+	            content[i][14] = elements.get(i)[14];
+	            content[i][15] = elements.get(i)[15];
+	            content[i][16] = elements.get(i)[16];
+	            content[i][17] = elements.get(i)[17];
+	            content[i][18] = elements.get(i)[18];
+	        }
+	        table.setModel(new DefaultTableModel(content,columNames));
+	        System.out.println(table.getModel().getValueAt(1, 1));
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	}
 }
